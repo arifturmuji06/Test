e348dbb4f23d993112621a6cf78848e2eb8336fc
yulistiocrisnandi <71747030+yulistiocrisnandi@users.noreply.github.com>
2020-12-26 10:41:41 +0700

    update card colom

--- cookies/app/Views/user/index.php
+++ cookies/app/Views/user/index.php
@@ -8,25 +8,33 @@
 
     <!-- User Detail -->
     <h2 class="h3 mb-4 text-gray-800">User Detail</h2>
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Nama User</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, perspiciatis provident autem exercitationem doloribus et ipsa non quas quos dignissimos dolor sapiente necessitatibus corrupti natus, quibusdam ducimus culpa perferendis repellendus..</p>
-        </div>
-        <ul class="list-group list-group-flush">
+    <div class="con">
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Nama User</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, perspiciatis provident autem exercitationem doloribus et ipsa non quas quos dignissimos dolor sapiente necessitatibus corrupti natus, quibusdam ducimus culpa perferendis repellendus..</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
 
-            <li class="list-group-item">Email</li>
-            <li class="list-group-item">Alamat</li>
-            <li class="list-group-item">Phone</li>
+                        <li class="list-group-item">Email</li>
+                        <li class="list-group-item">Alamat</li>
+                        <li class="list-group-item">Phone</li>
 
-        </ul>
-        <div class="card-body">
+                    </ul>
+                    <div class="card-body">
 
 
-            <a href="#" class="btn btn-primary">Edit Profile</a>
-            <td><button type="button" class="btn btn-danger">Delete</button></td>
+                        <a href="#" class="btn btn-primary">Edit Profile</a>
+                        <td><button type="button" class="btn btn-danger">Delete</button></td>
 
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <?= $this->endSection(); ?>
+</div>
+
+<?= $this->endSection(); ?>
