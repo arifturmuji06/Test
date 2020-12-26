62083bcafe21e242449ffb0e288bcd6087546307
yulistiocrisnandi <71747030+yulistiocrisnandi@users.noreply.github.com>
2020-12-26 10:25:35 +0700

    branchbarutio
    a

--- cookies/app/Views/user/index.php
+++ cookies/app/Views/user/index.php
@@ -8,6 +8,26 @@
 
     <!-- User Detail -->
     <h2 class="h3 mb-4 text-gray-800">User Detail</h2>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">User Profile</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Nama</li>
+            <li class="list-group-item">Email</li>
+            <li class="list-group-item">Alamat</li>
+            <li class="list-group-item">About</li>
+            <li class="list-group-item">Phonet</li>
+            <li class="list-group-item">Long</li>
+            <li class="list-group-item">Lat</li>
+        </ul>
+        <div class="card-body">
 
-</div>
-<?= $this->endSection(); ?>
+
+            <a href="#" class="btn btn-primary">Edit Profile</a>
+            <td><button type="button" class="btn btn-danger">Delete</button></td>
+
+        </div>
+        <?= $this->endSection(); ?>
--- cookies/app/Views/user/resep.php
+++ cookies/app/Views/user/resep.php
@@ -5,10 +5,38 @@
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800">Recipe Page</h1>
-
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Resep title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Edit Resep</a>
+            <td><button type="button" class="btn btn-danger">Delete Resep</button></td>
+        </div>
+    </div>
 
     <!-- Tabel list resep yang sudah live -->
     <h2 class="h3 mb-4 text-gray-800">List Resep</h2>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Serving</th>
+                <th scope="col">Duration</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Nasi Goreng</td>
+                <td>1 Porsi</td>
+                <td>1 Hari</td>
 
+            </tr>
+
+
+        </tbody>
+    </table>
 </div>
 <?= $this->endSection(); ?>
